77511659fb13bcdeb1fe2f188dfbc708fb071865
TariqNew <marandumashaka78@gmail.com>
2025-05-22 17:41:43 +0300

    feat(Navbar): Adding navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <h2>Better Tomorrow</h2>
+                </div>
+                <div className="links">
+                    <ul className="flex">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Programs</li>
+                        <li>Alumni</li>
+                        <li>Team</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
