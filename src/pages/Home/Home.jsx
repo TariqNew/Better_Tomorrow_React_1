2d210ef2160553fbc071d60bdf835cee5c842d05
TariqNew <marandumashaka78@gmail.com>
2025-05-23 23:52:50 +0300

    feat: creating the Home page with the navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
+import Button from "../../components/Button/Button";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Home.css";
+
 const Home = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div className="main-section">
+        <Navbar />
+        <div className="center-part flex flex-col items-center justify-center relative top-50">
+          <div className="large-txt text-5xl font-semibold text-white mb-4">Welcome to Better Tomorrow</div>
+          <div className="medium-txt text-2xl font-light text-white">Powering tomorrow, today</div>
+          <Button name='Explore our programs'/>
         </div>
-    )
-}
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
